bc8a257ab97c6c938ee1b27fca9853cb7a45c333
Jail_IB <jailsonsanches9@gmail.com>
2022-12-13 21:24:34 +0100

    feat: animationScreen

--- secret-password/src/components/StartScreen.js
+++ secret-password/src/components/StartScreen.js
@@ -4,8 +4,8 @@ import './StartScreen.css'
 const StartScreen = ({startGame}) => {
   return (
     <div className='start'>
-        <h1>Secret Word</h1>
-        <p> Click in the button to start Play</p>
+        <h1 data-text="Secret..Word">Secret..Word</h1>
+        <p data-text="Click in the button to start Play"> Click in the button to start Play</p>
         <button onClick={startGame}>Start the Game</button>
     </div>
   )
